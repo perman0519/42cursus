3a40b78f999197d0613b64add1bbc530667930a7
perman0519 <xms2007@naver.com>
2024-01-11 13:44:11 +0900

    rafact: cpp01/ex00

--- cpp_module/cpp01/ex00/newZombie.cpp
+++ cpp_module/cpp01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: junssong <junssong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:13:18 by junssong          #+#    #+#             */
-/*   Updated: 2024/01/08 16:29:26 by junssong         ###   ########.fr       */
+/*   Updated: 2024/01/11 13:42:40 by junssong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 Zombie*	newZombie( std::string name ) {
     Zombie *zombie = new Zombie(name);
-
     return zombie;
 }
--- cpp_module/cpp01/ex00/randomChump.cpp
+++ cpp_module/cpp01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: junssong <junssong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:13:12 by junssong          #+#    #+#             */
-/*   Updated: 2024/01/08 16:36:05 by junssong         ###   ########.fr       */
+/*   Updated: 2024/01/11 13:42:35 by junssong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,4 @@
 void	randomChump( std::string name ) {
     Zombie zombie(name);
     zombie.announce();
-    // zombie.~Zombie();
 }
