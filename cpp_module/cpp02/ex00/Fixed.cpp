5294740e6aec09533a0ab3221a0a3af221c442ae
perman0519 <xms2007@naver.com>
2024-01-11 15:14:19 +0900

    feat: cpp02/ex00

---
+++ cpp_module/cpp02/ex00/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junssong <junssong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 13:55:44 by junssong          #+#    #+#             */
+/*   Updated: 2024/01/11 14:21:24 by junssong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed &fixed) {
+     std::cout << "Copy constructor called" << std::endl;
+    if (this != &fixed)
+        this->_value = fixed.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+     std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &fixed)
+        this->_value = fixed.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+
+void Fixed::setRawBits( int const raw ) {
+    this->_value = raw;
+}
---
+++ cpp_module/cpp02/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junssong <junssong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 13:55:52 by junssong          #+#    #+#             */
+/*   Updated: 2024/01/11 14:19:02 by junssong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+public:
+    Fixed();
+    Fixed(Fixed &fixed);
+    Fixed &operator=(const Fixed &fixed);
+    ~Fixed();
+
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+
+private:
+    int _value; // An integer to store the fixed-point number _value.
+    static const int fractionalBits = 8;// A static constant integer to store the number of fractional bits.
+};
+
+
+#endif //FIXED_HPP
