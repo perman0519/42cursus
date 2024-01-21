955c51eb1622dd1d8cffa04366490444732ea9d1
perman0519 <102403228+perman0519@users.noreply.github.com>
2024-01-21 07:53:48 +0000

    feat: ex01

--- cpp_module/cpp02/ex01/Fixed.hpp
+++ cpp_module/cpp02/ex01/Fixed.hpp
@@ -26,7 +26,7 @@ public:
 
     Fixed &operator=(const Fixed &fixed);
 
-	const int getRawBits( void ) const;
+	int getRawBits( void ) const;
     void setRawBits( int const raw );
     float toFloat( void ) const;
     int toInt( void ) const;
