c2add1a2b9f845f33de1b44b4a1d714e6603683a
perman0519 <xms2007@naver.com>
2024-01-23 18:24:54 +0900

    feat: cpp04/ex00

---
+++ cpp_module/cpp04/ex00/Animal.hpp
@@ -0,0 +1,27 @@
+//
+// Created by Junsang Song on 1/23/24.
+//
+
+#ifndef CPP04_ANIMAL_HPP
+#define CPP04_ANIMAL_HPP
+
+#include <string>
+#include <iostream>
+
+class Animal {
+public:
+	Animal();
+	virtual ~Animal();
+	Animal(const Animal &animal);
+	Animal &operator=(const Animal &animal);
+
+	std::string getType() const;
+
+	virtual void makeSound() const ;
+
+protected:
+	std::string type;
+};
+
+
+#endif //CPP04_ANIMAL_HPP
---
+++ cpp_module/cpp04/ex00/Cat.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Junsang Song on 1/23/24.
+//
+
+#ifndef CPP04_CAT_HPP
+#define CPP04_CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+public:
+	Cat();
+	~Cat();
+	Cat(const Cat &cat);
+	Cat &operator=(const Cat &cat);
+
+	void makeSound() const ;
+
+private:
+};
+
+
+#endif //CPP04_CAT_HPP
