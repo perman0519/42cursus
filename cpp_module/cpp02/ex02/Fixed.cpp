c7b6d451931976e24ade4f2f8af5c7f630a0088b
perman0519 <102403228+perman0519@users.noreply.github.com>
2024-01-23 05:21:47 +0000

    feat: cpp02/ex02

--- cpp_module/cpp02/ex02/Fixed.cpp
+++ cpp_module/cpp02/ex02/Fixed.cpp
@@ -13,36 +13,36 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() : _value(0) {
-    std::cout << "Default constructor called" << std::endl;
+    // std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &fixed) {
-     std::cout << "Copy constructor called" << std::endl;
+    //  std::cout << "Copy constructor called" << std::endl;
     if (this != &fixed)
         this->_value = fixed.getRawBits();
 }
 
 Fixed::Fixed(const int integer) {
-    std::cout << "Int constructor called" << std::endl;
+    // std::cout << "Int constructor called" << std::endl;
     this->_value = integer << _fractionalBits;
 }
 
 Fixed::Fixed(const float number) {
-    std::cout << "Float constructor called" << std::endl;
+    // std::cout << "Float constructor called" << std::endl;
     float tmp = number * (1 << _fractionalBits);
     tmp = roundf(tmp);
     this->_value = tmp;
 }
 
 Fixed& Fixed::operator=(const Fixed &fixed) {
-     std::cout << "Copy assignment operator called" << std::endl;
+    //  std::cout << "Copy assignment operator called" << std::endl;
     if (this != &fixed)
         this->_value = fixed.getRawBits();
     return *this;
 }
 
 Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
+    // std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits( void ) const {
--- cpp_module/cpp02/ex02/main.cpp
+++ cpp_module/cpp02/ex02/main.cpp
@@ -10,26 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<iostream>
 #include "Fixed.hpp"
 
 int main(void) {
     Fixed       a;
-    Fixed const b(Fixed(5.05f) * Fixed(2));
+    Fixed const b(Fixed(5.05f) * Fixed(2.0f));
     Fixed const c(2.01f);
 
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl;
+
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
+    
     std::cout << b << std::endl;
+    std::cout << 5.05f * 2 << std::endl;
+
     std::cout << Fixed::max(a, b) << std::endl;
     std::cout << a << std::endl;
     std::cout << b << std::endl;
+
     std::cout << a - b << std::endl;
     std::cout << a * b << std::endl;
 
-    std::cout << "b * c = " << b * c << std::endl;
+    std::cout << "b * c = " << a * c << std::endl;
+    
     return 0;
 }
