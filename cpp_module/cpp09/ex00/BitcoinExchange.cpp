1784efc9d817d210bbca9d6b70ea5fe0e14345f8
perman0519 <xms2007@naver.com>
2024-04-10 12:25:09 +0900

    feat: cpp09/ex00 Leap year.

--- cpp_module/cpp09/ex00/BitcoinExchange.cpp
+++ cpp_module/cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: junssong <junssong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 15:15:03 by junssong          #+#    #+#             */
-/*   Updated: 2024/04/09 14:55:40 by junssong         ###   ########.fr       */
+/*   Updated: 2024/04/10 12:24:19 by junssong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,8 +113,16 @@ void	BitcoinExchange::help_run(std::string& line)
 			day =  (int)std::strtod(ss_str.c_str(), &err);
 			if (day <= 0 || *err)
 				throw FileError();
-			if (month == 2 && day >= 29)
-				throw FileError();
+			if (month == 2)
+			{
+				if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+				{
+					if (day >= 30)
+						throw FileError();
+				}
+				else if (day >= 29)
+					throw FileError();
+			}
 			else if ((month == 4 || month == 6 || month == 9 || month == 11) && day >= 31)
 				throw FileError();
 			else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
