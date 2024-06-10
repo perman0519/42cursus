e17f82a51631ca0199ad7c23aa56235449ad40f8
perman0519 <xms2007@naver.com>
2024-06-11 08:59:33 +0900

    feat: inception  clear

--- Inception/srcs/mariadb/conf/run-mariadb.sh
+++ Inception/srcs/mariadb/conf/run-mariadb.sh
@@ -1,75 +1,5 @@
 # #!/bin/sh
 
-# # create mysql
-# if [ -d "/run/mysql" ]; then
-# 	chown -R mysql:mysql /run/mysqld
-# else
-# 	mkdir -p /run/mysqld
-# 	chown -R mysql:mysql /run/mysqld
-# fi
-
-# # create mysql data directory
-# if [ -d /var/lib/mysql/mysql ]; then
-# 	echo "[INFO] mysql directory already present, skipping creation"
-# 	chown -R mysql:mysql /var/lib/mysql
-# else
-# 	echo "[INFO] mysql data directory not found, creating initial DBs"
-# 	chown -R mysql /var/lib/mysql
-# 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm > /dev/null
-# fi
-# 	# tfile=`mktemp`
-	# if [ ! -f "$tfile" ]; then
-	# 	return 1
-	# fi
-
-# 	cat << EOF > $tfile
-# USE mysql ;
-# FLUSH PRIVILEGES ;
-
-# DROP DATABASE IF EXISTS test ;
-
-# GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-# GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-# SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-# FLUSH PRIVILEGES ;
-
-# CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci ;
-# CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
-# CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
-# GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'localhost';
-# GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';
-# FLUSH PRIVILEGES ;
-
-# EOF
-
-# #UPDATE mysql.global_priv SET Host='%' WHERE Host='localhost' AND User='$WP_DB_USER' ;
-# #UPDATE mysql.db SET Host='%' WHERE Host='localhost' AND User='$WP_DB_USER' ;
-# #GRANT ALL PRIVILEGES ON '$WP_DB_NAME'.* TO '$WP_DB_USER'@'localhost';
-# #GRANT ALL PRIVILEGES ON '$WP_DB_NAME'.* TO '$WP_DB_USER'@'%';
-# 	ps -ef > /out_1.txt
-# 	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-# 	#/usr/bin/mysql root -p$MYSQL_ROOT_PASSWORD < $tfile
-# 	#rm -f $tfile
-# 	ps -ef > /out_2.txt
-# 	echo "[INFO] mysql init process done. Ready for start up."
-# fi
-
-# # allow remote connections
-# sed -i "s|.*skip-networking.*|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-# exec /usr/bin/mysqld --user=mysql --console
-# /usr/bin/mysql root -p$MYSQL_ROOT_PASSWORD < $tfile
-
-#!/bin/sh
-
-# 권한을 주기위해 mysqld의 경로를 미리 생성한다
-# if [ ! -d "/run/mysqld" ]; then
-# else
-# 	echo "Directory '/run/mysqld' already exist"
-# fi
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Directory '/run/mysqld' doesn't exist"
 	echo "Create '/run/mysqld' directory"
@@ -81,7 +11,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	# here_doc
 	cat << EOF > /bootstrap_commands
 
-USE MYSQL;
+USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE user='';
 
@@ -100,9 +30,7 @@ FLUSH PRIVILEGES ;
 
 EOF
 
-	# bootstrap 을 사용한 초기 설정
 	mysqld --bootstrap --user=$MYSQL_USER < /bootstrap_commands
-
 	rm -rf /bootstrap_commands
 else
 	echo "[INFO] mysql directory already present, skipping creation"
--- Inception/srcs/wordpress/conf/run_wordpress.sh
+++ Inception/srcs/wordpress/conf/run_wordpress.sh
@@ -1,34 +1,8 @@
 #!/bin/sh
 
-# if [ ! -d "/var/www/html/wordpress" ]; then
-# 	echo "hello"
-# 	echo "Directory "/var/www/html/wordpress" doesn't exist"
-# 	echo "Create "/var/www/html/wordpress" directory"
-# 	wget https://wordpress.org/latest.tar.gz
-# 	tar -xzf latest.tar.gz
-# 	rm latest.tar.gz
-# 	if [ ! -d "wordpress" ]; then
-# 		echo "WordPress directory not found"; exit 1;
-# 	fi
-
-# 	# Move WordPress to the desired location
-# 	mkdir -p /var/www/html
-# 	mv wordpress /var/www/html/wordpress
-
-# 	echo "listen = 9000" >> /etc/php81/php-fpm.d/www.conf
-# else
-# 	echo "Directory "/var/www/html/wordpress" already exist"
-# fi
-
-# wp-cli
-
-
-
 if [ -f "/var/www/html/wp-config.php" ]; then
 	echo "already installed."
 else
-	# mkdir -p /var/www/html/
-	# mkdir -p $WP_DIR
 	cd /var/www/html/
 	wp core download --allow-root
 
@@ -38,8 +12,9 @@ else
 
 	wp theme install hestia --activate --allow-root
 
-	# 유저 생성
 	wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW --allow-root
 fi
+
 chown -R nginx:nginx /var/www/html
+
 php-fpm81 -F
