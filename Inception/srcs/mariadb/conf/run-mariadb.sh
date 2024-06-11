3ccd22315f23e0987dd0b15151e539618a8f7b99
perman0519 <xms2007@naver.com>
2024-06-11 14:03:52 +0900

    feat: inception  clear

--- Inception/srcs/mariadb/conf/run-mariadb.sh
+++ Inception/srcs/mariadb/conf/run-mariadb.sh
@@ -1,14 +1,11 @@
-# #!/bin/sh
+#!/bin/sh
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "Directory '/run/mysqld' doesn't exist"
-	echo "Create '/run/mysqld' directory"
+	echo "[INFO] Create '/run/mysqld' directory."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
-	# mysql 초기 설치
 	mariadb-install-db --user=$MYSQL_USER --datadir=/var/lib/mysql --basedir=/usr --rpm > /dev/null
 
-	# here_doc
 	cat << EOF > /bootstrap_commands
 
 USE mysql;
@@ -16,15 +13,11 @@ FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE user='';
 
 GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PW' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PW' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$MYSQL_ROOT_PW') ;
 SET PASSWORD FOR 'root'@'%'=PASSWORD('$MYSQL_ROOT_PW') ;
 FLUSH PRIVILEGES ;
 
 CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci ;
-CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PW';
 CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PW';
-GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';
 FLUSH PRIVILEGES ;
 
@@ -33,7 +26,7 @@ EOF
 	mysqld --bootstrap --user=$MYSQL_USER < /bootstrap_commands
 	rm -rf /bootstrap_commands
 else
-	echo "[INFO] mysql directory already present, skipping creation"
+	echo "[INFO] Directory already present, skipping creation"
 	chown -R mysql:mysql /var/lib/mysql
 fi
 
--- Inception/srcs/wordpress/conf/run_wordpress.sh
+++ Inception/srcs/wordpress/conf/run_wordpress.sh
@@ -4,15 +4,11 @@ if [ -f "/var/www/html/wp-config.php" ]; then
 	echo "already installed."
 else
 	cd /var/www/html/
-	wp core download --allow-root
 
-	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PW --dbhost=$MYSQL_HOST --allow-root
-
-	wp core install --url=$DOMAIN --title="Junssong's INCEPTION" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --allow-root
-
-	wp theme install hestia --activate --allow-root
-
-	wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW --allow-root
+	wp core download
+	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PW --dbhost=$MYSQL_HOST
+	wp core install --url=$DOMAIN --title="Junssong's INCEPTION" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL
+	wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW
 fi
 
 chown -R nginx:nginx /var/www/html
