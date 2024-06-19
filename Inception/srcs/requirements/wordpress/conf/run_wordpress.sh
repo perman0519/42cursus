fe70780ad2e40a2a8871aff5ef9ac4cf643f8156
Your Name <you@example.com>
2024-06-19 17:22:59 +0900

    feat: wp theme install zakra

--- Inception/srcs/requirements/wordpress/conf/run_wordpress.sh
+++ Inception/srcs/requirements/wordpress/conf/run_wordpress.sh
@@ -9,6 +9,7 @@ else
 	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PW --dbhost=$MYSQL_HOST
 	wp core install --url=$DOMAIN --title="Junssong's INCEPTION" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL
 	wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW
+	wp theme install zakra --activate
 	chown -R nginx:nginx /var/www/html
 fi
 
