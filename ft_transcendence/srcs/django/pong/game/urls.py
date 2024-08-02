321803aed4988d29471b43868c15d210e9afbb3e
junssong <xms2007@naver.com>
2024-08-02 17:44:16 +0900

    delete: django

--- ft_transcendence/srcs/django/pong/game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import game
-
-
-urlpatterns = [
-    path('game', game.GameView.as_view(), name='game'),
-    path('tournament', game.TournamentView.as_view(), name='tournament'),
-]
--- ft_transcendence/srcs/django/pong/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python manage.py makemigrations
-python manage.py makemigrations game
-python manage.py makemigrations authentication
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 --workers 3 --worker-class uvicorn.workers.UvicornWorker pong.asgi:application
